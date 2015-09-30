e36a04641df8e7bcd7ffda0fc120c47484e9b45e
Yun Li <muziriyun@gmail.com>
2015-09-30 12:11:48 +0800

    Add printable resume and pdf version

--- gen-resume.sh
+++ gen-resume.sh
@@ -6,6 +6,9 @@ MD_FILE="resume.md"
 CONTENT_FILE="resume-content.html"
 TEMPLATE_FILE="resume-template.html"
 RESUME_FILE="resume.html"
+PRINTABLE_TEMPLATE_FILE="resume-template-printable.html"
+PRINTABLE_RESUME_FILE="resume-printable.html"
 
 $MARKDOWN $MD_FILE > $CONTENT_FILE
 $SED -e "/{{ content }}/{r $CONTENT_FILE" -e "d}" $TEMPLATE_FILE > $RESUME_FILE
+$SED -e "/{{ content }}/{r $CONTENT_FILE" -e "d}" $PRINTABLE_TEMPLATE_FILE > $PRINTABLE_RESUME_FILE
